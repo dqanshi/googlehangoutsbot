f62c9aaf8fef703c198fb17d81f93b2b070a5387
Jason Kucharski <jasonok@protonmail.com>
2017-10-26 15:57:46 -0500

    Change the /bot command to be more complex, so we arent discovered by someone typing that for some reason

--- hangupsbot/handlers.py
+++ hangupsbot/handlers.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 class EventHandler:
     """Handle Hangups conversation events"""
 
-    def __init__(self, bot, bot_command='/bot'):
+    def __init__(self, bot, bot_command='/botbotbot6433'):
         self.bot = bot
         self.bot_command = bot_command
 
@@ -61,8 +61,8 @@ class EventHandler:
         return _id
 
     def attach_reprocessor(self, callable, return_as_dict=False):
-        """reprocessor: map callable to a special hidden context link that can be added anywhere 
-        in a message. when the message is sent and subsequently received by the bot, it will be 
+        """reprocessor: map callable to a special hidden context link that can be added anywhere
+        in a message. when the message is sent and subsequently received by the bot, it will be
         passed to the callable, which can modify the event object by reference
         """
         _id = self.register_reprocessor(callable)
@@ -191,7 +191,7 @@ class EventHandler:
                 yield from self.bot.coro_send_message(event.conv, _('{}: Missing parameter(s)').format(
                     event.user.full_name))
             return
-        
+
         commands = command.get_available_commands(self.bot, event.user.id_.chat_id, event.conv_id)
 
         supplied_command = line_args[1].lower()
